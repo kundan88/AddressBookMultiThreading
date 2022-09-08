839e44fb65c2f52ffe7f5a7a58a603d1497273d2
Kundan <kundan kamble>
2022-09-08 14:30:26 -0800

    With Threads

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -24,10 +24,17 @@ namespace AddressBookTest
             AddressList.Add(new AddressBook(Firstname: "Rita", Lastname: "Harward", Address: "Buckhimgam", City: "London", State: "NewYork", Zip: 147856, PhoneNumber: 7742365898, Email: "harwardlucifer@gmail.com"));
             AddressList.Add(new AddressBook(Firstname: "Robbert", Lastname: "Escobar", Address: "BeverlyHills", City: "SanAndreas", State: "Washington", Zip: 136987, PhoneNumber: 8846932568, Email: "pablo@gmail.com"));
 
+            // Without Thread
             DateTime StartDateTime = DateTime.Now;
             addressBookOperations.AddContacts(AddressList);
             DateTime StopDateTimes = DateTime.Now;
             Console.WriteLine("Duration without threads: " + (StopDateTimes - StartDateTime));
+
+            //With Thread
+            DateTime StartDateTimeThread = DateTime.Now;
+            addressBookOperations.AddContactsThread(AddressList);
+            DateTime StopDateTimeThread = DateTime.Now;
+            Console.WriteLine("Duration with threads: " + (StopDateTimeThread - StartDateTimeThread));
         }
     }
 }
